34abf6e0bae4a2b5984b5c1ec60e1f78e584a994
ropi59 <mrseed@hotmail.com>
2022-12-08 09:03:12 +0100

    initial commit

---
+++ src/test/java/fr/olivier/formationcda/ecf4/localib/LocalibApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.olivier.formationcda.ecf4.localib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
